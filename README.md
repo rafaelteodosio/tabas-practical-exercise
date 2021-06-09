cd8663d4e99384d773571d9c63d9cb61d1a5db30
Rafael Teodosio <rafzteodosio@gmail.com>
2021-06-08 23:48:59 -0300

    add README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# tabas-practical-exercise
+# Tabas Practical Exercise
+
+## Author: Rafael Santos Teodosio
+
+### To execute the exercise just open the `index.html` file.
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,31 @@
 function submitForm() {
+  // GETTING VALUES FROM THE FORM
   var text = document.getElementById("textID").value;
   var select = document.getElementById("selectID").value;
   var checkbox = document.getElementById("checkboxID").checked;
   var date = document.getElementById("dateID").value;
+  //
+
+  //PUSH FORM VALUES INTO AN ARRAY
   var allItens = [];
   allItens.push(text, select, checkbox, date);
+  //
+
+  //CREATING HTML ELEMENTS
   var listDiv = document.createElement("div");
   var listItem = document.createElement("li");
-  listItem.setAttribute("class", "list-group-item");
   var currentList = document.getElementById("itemlist");
+  //
+
+  //APPENDING THE FORM VALUES INTO THE LIST
   for (const item of allItens) {
     listItem = document.createElement("li");
-    listItem.setAttribute("class", "list-group-item");
     listItem.innerText = item;
     listDiv.append(listItem);
   }
   currentList.appendChild(listDiv);
-  console.log("currentList :>> ", currentList);
-  return false; // stop submission
+  //
+
+  //PREVENT THE SUBMITION
+  return false;
 }
